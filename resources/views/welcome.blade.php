6fa9c0ebe92b9b3597562e71e0d8d0d3b946bfaf
Theofilos Intzoglou <int.teo@gmail.com>
2026-09-01 09:43:28 +0000

    Διαμόρφωση αρχικής σελίδας για καλύτερη εμφάνιση

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
     <title>Σχολικές Εκδρομές-Σύνδεση</title>
 
     @vite(['resources/css/app.css'])
-    <link href="{{ asset('js/datatables.min.css') }}" rel="stylesheet">
 </head>
 
 <body class="font-sans">
 
     <p class="text-center mt-4">
-        <a href="http://dide-v.thess.sch.gr" title="http://dide-v.thess.sch.gr" class="text-coral hover:underline">
+        <a href="https://srv-dide-v.thess.sch.gr" title="https://srv-dide-v.thess.sch.gr"
+            class="text-coral hover:underline">
             Διεύθυνση Δευτεροβάθμιας Εκπαίδευσης Δυτικής Θεσσαλονίκης
         </a>
     </p>
@@ -31,30 +31,21 @@
         </div>
     @endsession
 
-    <form id="identity" action="{{ route('dashboard') }}" class="flex flex-col items-center mt-8">
-        <table class="bg-white">
-            <tr>
-                <td colspan="2" class="text-center">
-                    <img src="{{ Vite::asset('resources/images/icons8-bus.gif') }}" width="128" height="128"
-                        class="mx-auto">
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" class="text-center">
-                    <h4 class="text-lg">Σχολικές Εκδρομές</h4>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">&nbsp;</td>
-            </tr>
-            <tr>
-                <td colspan="2" class="text-center">
-                    <u>Απαιτείται πιστοποίηση χρήστη:</u><br>
-                    Χρησιμοποιήστε το λογαριασμό του σχολείου στο<br>
-                    Πανελλήνιο Σχολικό Δίκτυο για να συνδεθείτε
-                </td>
-            </tr>
-        </table>
+    <form id="identity" action="{{ route('dashboard') }}" class="flex flex-col items-center">
+        <div class="flex flex-col bg-white py-8 px-16 rounded-lg shadow-lg my-8">
+            <div class="text-center">
+                <img src="{{ Vite::asset('resources/images/icons8-bus.gif') }}" width="128" height="128"
+                    class="mx-auto">
+            </div>
+            <div class="text-center py-4">
+                <h4 class="text-lg">Σχολικές Εκδρομές</h4>
+            </div>
+            <div class="text-center">
+                <u>Απαιτείται πιστοποίηση χρήστη:</u><br>
+                Χρησιμοποιήστε το λογαριασμό του σχολείου στο<br>
+                Πανελλήνιο Σχολικό Δίκτυο για να συνδεθείτε
+            </div>
+        </div>
         <p class="mt-4">
             <input type="submit" value="Σύνδεση" name="submitButton" id="submitButton"
                 class="text-lg px-6 py-2 bg-coral text-white rounded hover:bg-coral-dark cursor-pointer">
@@ -62,12 +53,7 @@
         <br>
     </form>
 
-    <p class="text-center mt-4 text-sm text-gray-500">
-        Εάν η εφαρμογή δεν αποκρίνεται, δοκιμάστε λίγα λεπτά αργότερα.<br>
-        Εάν αντιμετωπίσετε κάποιο πρόβλημα επικοινωνήστε με το τμήμα Πληροφορικής της Δ/νσης
-    </p>
-
-    <p class="text-center mt-4 text-xs text-gray-400">
+    <p class="text-center text-xs text-gray-500 fixed bottom-0 w-full mb-2">
         <em>Τμήμα Πληροφορικής ΔΔΕ Δυτ. Θεσσαλονίκης &copy; 2023-{{ date('Y') }}</em>
     </p>
 </body>
