98b3eda3eeee67c2ea57b2ebc3057390283a1dab
Theofilos Intzoglou <int.teo@gmail.com>
2026-09-15 16:59:25 +0000

    Προσθήκη κλεισίματος εισαγωγικών που έλειπε από την τιμή του component

--- resources/views/components/excursion/form/ui/time.blade.php
+++ resources/views/components/excursion/form/ui/time.blade.php
@@ -5,5 +5,5 @@
         class="block text-sm font-medium text-gray-700 mb-1">{{ $fieldDef['label'] }}{{ $required ? ' *' : '' }}</label>
     <input type="time" name="{{ $fieldName }}" id="{{ $fieldName }}"
         class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-coral focus:border-transparent"
-        {{ $required ? 'required' : '' }} value="{{ $value }}>
+        {{ $required ? 'required' : '' }} value="{{ $value }}">
 </div>
