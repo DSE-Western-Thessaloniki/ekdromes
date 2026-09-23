6280051d723f1eb47c5b145487829dabb3c0e656
Theofilos Intzoglou <int.teo@gmail.com>
2026-09-23 13:11:33 +0000

    Επέτρεψε την εφαρμογή να ξεκινήσει από υποφάκελο του server

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,58 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import { bunny } from "laravel-vite-plugin/fonts";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: ["resources/css/app.css", "resources/ts/app.ts"],
-      refresh: true,
-      fonts: [
-        bunny("Instrument Sans", {
-          weights: [400, 500, 600],
-        }),
-      ],
-      assets: ["resources/images/**", "resources/css/transmittal.css"],
-    }),
-    tailwindcss(),
-  ],
-  server: {
-    watch: {
-      ignored: ["**/storage/framework/views/**"],
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
+
+  // Εύρεση resource path από το APP_URL
+  var resources_dir;
+  var index = 0;
+  index = process.env.APP_URL.indexOf("//");
+  if (index === -1) {
+    console.log("Σφάλμα με τον ορισμό του APP_URL στο .env!");
+    process.exit(1);
+  }
+  index = process.env.APP_URL.indexOf("/", index + 2);
+  if (index === -1) {
+    resources_dir = "/";
+  } else {
+    resources_dir = process.env.APP_URL.substring(index);
+    if (!resources_dir.endsWith("/")) {
+      resources_dir += "/";
+    }
+  }
+
+  console.log("==", resources_dir);
+  process.env.ASSET_URL = resources_dir;
+
+  return {
+    plugins: [
+      laravel({
+        input: ["resources/css/app.css", "resources/ts/app.ts"],
+        refresh: true,
+        fonts: [
+          bunny("Instrument Sans", {
+            weights: [400, 500, 600],
+          }),
+        ],
+        assets: ["resources/images/**", "resources/css/transmittal.css"],
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      watch: {
+        ignored: ["**/storage/framework/views/**"],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "resources/ts"),
+        "~@fortawesome": path.resolve(__dirname, "node_modules/@fortawesome"),
+        "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
+      },
     },
-  },
+  };
 });
